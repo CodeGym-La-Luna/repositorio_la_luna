04d0131e34a9da3e53a0e75c6029d1a15656584f
Rodrigo Ochoa <rodrigo.ochoa.ruiz.19@gmail.com>
2024-09-27 21:42:14 -0600

    se agrega metodo a clase estudiante y rama

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -3,6 +3,12 @@ public class Estudiante {
     private String apellido;
     private String pais;
 
+    public Estudiante(String nombre, String apellido, String pais) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.pais = pais;
+    }
+
     public String getNombre() {
         return nombre;
     }
@@ -25,5 +31,10 @@ public class Estudiante {
 
     public void setPais(String pais) {
         this.pais = pais;
+    }
+
+    public void printMensaje(String mensaje){
+        System.out.println(mensaje);
+        System.out.println("De: "+this.nombre+" "+this.apellido+" desde "+this.pais);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Estudiante e7=new Estudiante("Rodrigo","Ochoa","México");
+        e7.printMensaje("Primero que nada, felicidades por su asenso a Manager, la vi muy entretenida dirigiéndonos" +
+                " este juego fue un excelente ejercicio, agradezco su paciencia, energía y alegría en las clases" +
+                "porque esto me ha echo permanecer en CodeGym. Seguimos en contacto, le escribiré un email!!!");
     }
 }
